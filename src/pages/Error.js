c57ddf19b19834e0c2ea611ed018608645776d6f
midCenturyDude7 <mgriffes@gmail.com>
2023-03-16 11:25:13 -0700

    Added Error component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <section className='section'>
-      <h2>Error</h2>
+    <section className="section">
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to="/" className="btn">
+        Home
+      </Link>
+      <Link to="/about" className="btn">
+        About
+      </Link>
+      <Link to="/products" className="btn">
+        Products
+      </Link>
     </section>
   );
 };
