679bf28f24205372661b1ff514d1118051efc1d0
midCenturyDude7 <mgriffes@gmail.com>
2023-03-16 16:52:54 -0700

    Added a login page via a ProtectedRoute component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import SharedProductLayout from "./pages/SharedProductLayout";
 
 function App() {
   const [user, setUser] = React.useState(null);
@@ -18,23 +20,23 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:productId" element={<SingleProduct />} />
+
+          <Route path="/products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
+
           <Route path="/login" element={<Login setUser={setUser}></Login>} />
           <Route
             path="/dashboard"
-            element={<Dashboard user={user}></Dashboard>}
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user}></Dashboard>
+              </ProtectedRoute>
+            }
           />
           <Route path="*" element={<Error />} />
         </Route>
-        <Route
-          path="/testing"
-          element={
-            <div>
-              <h2>Testing Page</h2>
-            </div>
-          }
-        />
       </Routes>
     </>
   );
--- src/pages/SharedProductLayout.js
+++ src/pages/SharedProductLayout.js
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import StyledNavbar from "../components/StyledNavbar";
 
 const Home = () => {
   return (
     <>
-      <StyledNavbar />
       <Outlet />
     </>
   );
