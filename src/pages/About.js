a641b6a613dfa26bc65e8b413a6df1f2c3f8c255
midCenturyDude7 <mgriffes@gmail.com>
2023-03-16 11:18:03 -0700

    Navigating site with Link

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <section className="section">
       <h2>About Page</h2>
+      <Link to="/" className="btn">
+        Home
+      </Link>
+      <Link to="/products" className="btn">
+        Products
+      </Link>
     </section>
   );
 };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <>
       <section className="section">
         <h2>Products Page</h2>
+        <Link to="/" className="btn">
+          Home
+        </Link>
+        <Link to="/about" className="btn">
+          About
+        </Link>
       </section>
     </>
   );
